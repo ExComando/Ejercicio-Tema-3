5c58c590f1954a761615e3d37ec497fe7724d8bc
ExComando <79213925+ExComando@users.noreply.github.com>
2022-04-25 09:24:21 -0300

    Add files via upload

---
+++ Basico_1.java
@@ -0,0 +1,43 @@
+
+package ejercicios_basicos;
+
+import java.nio.file.FileSystemNotFoundException;
+
+public class Basico_1 {
+ 
+    public static void main(String[] args) {
+        
+        // Enteros
+        
+        byte num = 3;
+        short num1 = 23;
+        int num2 = 2;
+        long num3 = 452658555;
+        
+        // Decimales
+        
+        double num4 = 9.6;
+        float num5 = 10.7F;        
+        
+        //Booleano
+        
+        boolean trabajadorActivo = true;
+        
+        // texto 
+        
+        String nombre = "Me llamo Dardo";
+        Character letra = 'a';  
+        
+        
+        System.out.println(num + "\n" + num1 + "\n" + num2 + "\n" + num3 + 
+                "\n" + num4 + "\n" + num5 + "\n" + trabajadorActivo + "\n" + nombre + "\n" + letra );
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
