981b309d9319cf0601d5487a78ec546e0f8c018a
ExComando <79213925+ExComando@users.noreply.github.com>
2022-04-29 14:08:57 -0300

    Add files via upload

---
+++ Sesion_3.java
@@ -0,0 +1,22 @@
+//En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+//Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
+
+package openbootcam;
+
+public class Sesion_3 {
+
+    public static void main(String[] args) {
+        
+        
+        String[] sumaStrings = {"DARDO ", "DARIO ", "CALLADO"};
+
+        for (int i = 0; i < sumaStrings.length; i++) {
+            
+           
+            System.out.print(sumaStrings[i]);
+
+        }
+
+    }
+
+}
