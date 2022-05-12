a9c881040e877f8b9d7ace0f9af4727740e98295
ExComando <79213925+ExComando@users.noreply.github.com>
2022-05-12 17:40:31 -0300

    Add files via upload

---
+++ CocheCRUDImpl.java
@@ -0,0 +1,28 @@
+
+package openbootcam;
+
+
+public class CocheCRUDImpl implements CocheCRUD{
+
+    @Override
+    public void save() {
+        
+        System.out.println("save");
+    }
+
+    @Override
+    public void findAll() {
+        
+        System.out.println("findALL");
+         
+    }
+
+    @Override
+    public void delete() {
+        
+        System.out.println("delete");
+         
+    }  
+    
+    
+}
